3a8c4041421d331b0c22e487286289f3d8099c48
Yerfdog1990 <gouma308@gmail.com>
2024-07-29 07:55:17 +0300

    Cook Your Lasagna

---
+++ src/Challenge1/Instructions.java
@@ -0,0 +1,16 @@
+package Challenge1;
+/*
+Instructions
+In this exercise you're going to write some code to help you cook a brilliant lasagna from your favorite cooking book.
+You have four tasks, all related to the time spent cooking the lasagna.
+1.Define the expected oven time in minutes
+    -TODO: define the 'expectedMinutesInOven()' method
+2.Calculate the remaining oven time in minutes
+    -TODO: define the 'remainingMinutesInOven()' method
+3.Calculate the preparation time in minutes
+    -TODO: define the 'preparationTimeInMinutes()' method
+4.Calculate the total working time in minutes
+-   TODO: define the 'totalTimeInMinutes()' method
+ */
+public class Instructions {
+}
